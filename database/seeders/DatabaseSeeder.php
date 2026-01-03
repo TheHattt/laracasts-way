704045e6923c0cbe9ec1292f866cf94679d341d0
talyx <ngwemoa@gmail.com>
2026-01-03 23:10:26 +0300

    At Glance: Factories and Database Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,8 +19,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Tally',
+            'last_name' => 'Ho',
             'email' => 'test@example.com',
         ]);
+
+    $this->call(JobSeeder::class);
+
     }
 }
